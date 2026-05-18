8b03b87782bd888e0ed9b05cfd91ed1b9139e45a
vinay <vinaybarrenkula@gmail.com>
2026-05-18 13:46:47 +0530

    Update RootLayout.jsx

--- frontend/src/components/RootLayout.jsx
+++ frontend/src/components/RootLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router'
 
 function RootLayout() {
   return (
-    <div className='bg-linear-to-t from-blue-200 to-purple-300'>
+   <div className='bg-gradient-to-t from-orange-400 to-pink-200'>
         <Header />
         <div className='min-h-screen mx-0 sm:mx-5 md:mx-10 lg:mx-20 p-20 bg-linear-to-t from-blue-200 to-purple-300'>
             <Outlet />
