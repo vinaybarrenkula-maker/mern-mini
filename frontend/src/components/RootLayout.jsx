189694f0dff40db7d395964c01425fa30d5cfdad
vinay <vinaybarrenkula@gmail.com>
2026-05-18 13:54:59 +0530

    Change background gradient in RootLayout component

--- frontend/src/components/RootLayout.jsx
+++ frontend/src/components/RootLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router'
 
 function RootLayout() {
   return (
-   <div className='bg-gradient-to-t from-orange-400 to-pink-200'>
+   <div className='bg-gradient-to-t from-cyan-300 to-sky-100'>
         <Header />
         <div className='min-h-screen mx-0 sm:mx-5 md:mx-10 lg:mx-20 p-20 bg-gradient-to-t from-cyan-500 via-sky-300 to-blue-100'>
             <Outlet />
